bca6d8590f3259e68cfc8c21796adb99bdd22d56
Yuragranovsky <yuragranovsky@gmail.com>
2022-12-14 18:02:50 +0200

    hide details

--- src/dogs.js
+++ src/dogs.js
@@ -1,10 +1,26 @@
 const detailsImage = document.querySelector(".details-image");
 const detailsTitle = document.querySelector(".details-title");
 const anchorElements = document.querySelectorAll(".thumbnails-anchor")
+const mainElement = document.querySelector(".main-class");
+const hideButtonElement = document.getElementById("hide-button");
+const HIDDEN = "hidden";
+function showDetails() {
+    mainElement.classList.remove(HIDDEN);
+}
+function hideDetails() {
+    mainElement.classList.add(HIDDEN);
+}
 function setDetails(anchor) {
     const dataImage = anchor.getAttribute("data-details-image");
     detailsImage.src = dataImage;
+    showDetails();
     detailsTitle.innerHTML = anchor.getAttribute("data-details-title");
+    const color = anchor.getAttribute("data-text-color");
+    if (color) {
+        detailsTitle.style.color = color;
+    } else {
+        detailsTitle.style.color = '';
+    }
 
 }
 for (let i = 0; i < anchorElements.length; i++) {
@@ -12,4 +28,5 @@ for (let i = 0; i < anchorElements.length; i++) {
         setDetails(anchorElements[i]);
     } )
 }
+hideButtonElement.addEventListener("click",hideDetails);
 
