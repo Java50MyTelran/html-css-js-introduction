04ac1392dbe67d6a62592013c6fe409c695a4c77
Yuragranovsky <yuragranovsky@gmail.com>
2022-12-16 11:31:26 +0200

    CW #10 Details Container animation

--- src/dogs.js
+++ src/dogs.js
@@ -1,11 +1,18 @@
 const detailsImage = document.querySelector(".details-image");
 const detailsTitle = document.querySelector(".details-title");
+const detailsContainer = document.querySelector(".details-container");
 const anchorElements = document.querySelectorAll(".thumbnails-anchor")
 const mainElement = document.querySelector(".main-class");
 const hideButtonElement = document.getElementById("hide-button");
 const HIDDEN = "hidden";
+const IS_POINT = "is-point";
 function showDetails() {
     mainElement.classList.remove(HIDDEN);
+    detailsContainer.classList.add(IS_POINT);
+    setTimeout(function() {
+        detailsContainer.classList.remove(IS_POINT);
+    })
+
 }
 function hideDetails() {
     mainElement.classList.add(HIDDEN);
