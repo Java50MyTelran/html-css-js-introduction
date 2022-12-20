d6043d0aaa6e5408a1ee8134f19db67de5c33aed
Yuragranovsky <yuragranovsky@gmail.com>
2022-12-20 21:06:31 +0200

    CW #11 JS primitives and conversions

--- app.js
+++ app.js
@@ -1,4 +1,94 @@
-console.log("hello world!");
-let a=10;
-let b=20;
-let c=a + b;
+//numbers
+// let num = 25.325;
+// num = 40.5;
+
+//num = Math.floor(num);
+//num = Math.trunc(num);
+//num = Math.ceil(num);
+// num = Math.round(num);
+// num **= 2; // num = num ** 2
+
+//strings
+// let str = "a'bc'd";
+// let str1 = 'a"bc"d';
+// let res = +"123" + +56;
+// let res1 = "123" - 56;
+// let res2 = Math.round("123" / 56);
+// let res3 = "123" / "2";
+// let res4 = "ab" / 8;
+
+//boolean
+// let a = 100;
+// let error = true;
+// let res = true + true;
+// let res1 = true && false;
+// let res2 = a || 256 / a;
+// let message = error && "some error happenning";
+// let re3 = a || 10;
+// let res4 = "123" > "9";
+// let res5 = "123" > 9;
+//equilty operators
+let res10 = "123" == 123; //true
+let res11 = "123" === 123; //false
+//string functions for HW
+let str = "aBdTYg";
+str = str.toLowerCase(); //"abcdtyg"
+
+// HW #11
+function getDigitsSum(number) {
+    //computes sum of all digits in the integer part of the number
+    // number may be either negative or positive or 0
+}
+function computeExpression(expressionStr) {
+    //example of expression "9000 / ((10 + 20) ** 2)"
+    //returns 10
+    //task for searching in Internet
+    //only one line code
+    //how to use Internet for finding required code
+}
+//console.log(computeExpression("9000 / ((10 + 20) ** 2)")) ; //prints 10
+//console.log(computeExpression("9 + 100 / 2")); //prints 59
+function printAnanas() {
+    //TODO 
+    //To use only capital "A" "S"
+    //print "ananas"
+}
+printAnanas() //there should be printed "ananas"
+
+function reverse(number) {
+    //returns string with digits of a given number in the reversed order
+    //number may be either positive or negative
+    //consider only integer part, fractional part should be removed
+    //example reverse(1234.56) should return "4321"
+    //example reverse (-1234) should return "-4321"
+}
+let n = 10;
+let count = 0;
+// while (n > 0) {
+//     n--;
+//     count++;
+// }
+// do {
+//   n--;
+//   count++
+// } while(n > 0);
+while(--n) {
+    count++;
+}
+let d;
+// 3 + 7 * "10"
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
