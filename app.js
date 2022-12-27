4fa396e9a9b60257b9790c3ebd3e59d7d11b825d
Yuragranovsky <yuragranovsky@gmail.com>
2022-12-27 19:04:01 +0200

    HW #12 solution

--- app.js
+++ app.js
@@ -1,72 +1,100 @@
-// let str = "123m"
-// let num = parseInt(str) + 10 + 'm';
-// let str1 = "js.5";
-// let numInt = parseInt(str1, 32);
-// let numFloat = parseFloat(str1);
+
 function myParseInt(str, base) {
     base = base || 10;
-    let res = 0;
-    for(let i = 0; i < str.length; i++) {
-            res = res * base + getCode(str[i]);
+    let res = NaN;
+    const firstIndex = str[0] === "-" ? 1 : 0;
+    let code = 1;
+    let index = firstIndex;
+    code = getCode(str[index], base);
+   if(code >= 0) {
+    res = code;
+    index++;
+   }
+    while (index < str.length && code >= 0) {
+        code = getCode(str[index], base);
+        if (code >= 0) {
+            res = res * base + code;
+        }
+        index++;
+
+    }
+    if (firstIndex === 1) {
+        res = -res;
     }
     return res;
 }
-function getCode(symbol) {
-    symbol = symbol.toLowerCase();
+function getCode(symbol, base) {
+    let res = -1; 
+    symbol = symbol.toLowerCase(); 
     const codeA = 'a'.charCodeAt();
-    const res = symbol <= '9'? +symbol : symbol.charCodeAt() - codeA + 10;
+    if((symbol <= '9' && symbol >= '0') || (symbol >= 'a' && symbol <= 'z')) {
+        res = symbol <= '9' ? +symbol : symbol.charCodeAt() - codeA + 10;
+        if (res >= base) {
+            res = -1;
+        }
+    }
+   
+   
+    
     return res;
 }
-// let str1 = "ff";
-// let str2 = "123";
-// let str22 = "Java";
-// let str3 = "123m"
-// let str4 = "123.5"
-// let num = parseInt(str1, 16);
-// let myNum = myParseInt(str1, 16);
-// num = parseInt(str2);
-// myNum = myParseInt(str2);
-// num = parseInt(str22, 36);
-// myNum = myParseInt(str22, 36);
-// num = parseInt(str3);
-// myNum = myParseInt(str3);
-// num = parseInt(str4);
-// myNum = myParseInt(str4);
-// let number = 255;
-// let str = "" + number;
-// str = number.toString(36);
+
 function myToString(number, base) {
     let res = '';
+
     base = base || 10;
-    number = Math.trunc(Math.abs(number));
+    const sign = number < 0 ? "-" : "";
+    const integerPart = Math.trunc(Math.abs(number));
+    const fractinPart = number - integerPart;
+    res = convertIntegerPart(integerPart, base) + '.' +
+        convertFraictionPart(fractinPart, base);
+   
+    return sign + res;
+
+}
+function convertIntegerPart(number, base) {
+    let res = '';
     do {
         const digit = number % base;
         const symbol = getSymbol(digit);
         res = symbol + res;
         number = Math.trunc(number / base);
 
-
-    }while(number);
+    } while (number);
     return res;
+}
+function convertFraictionPart(number, base, precision) {
+    let res = '';
+    precision = precision || 7
+    let count = 0;
+    let intPart;
+    do {
+        
+        number *= base;
+        intPart = Math.trunc(number);
+        if (intPart != 0) {
+            if (count == precision && intPart > base / 2) {
+                intPart++;
+            }
+            const symb = getSymbol(intPart);
+            res += symb;
+            number = number - intPart;
+        }
+        count++;
 
+    } while (intPart && count <= precision);
+    return res;
 }
 function getSymbol(digit) {
     const codeA = 'a'.charCodeAt();
     let symbol;
-   if(digit < 10) {
-    symbol = "" + digit;
-   } else {
-     const codeAscii = digit - 10 + codeA;
-     symbol = String.fromCharCode(codeAscii);
-   }
-   return symbol;
+    if (digit < 10) {
+        symbol = "" + digit;
+    } else {
+        const codeAscii = digit - 10 + codeA;
+        symbol = String.fromCharCode(codeAscii);
+    }
+    return symbol;
 
 }
-let num100 = 990500;
-let str100 = num100.toString();
-let myStr100 = myToString(num100);
-str100 = num100.toString(36);
-myStr100 = myToString(num100, 36);
-num100 = 123.45;
-str100 = num100.toString(16);
-myStr100 = myToString(num100, 16);
+console.log(myParseInt("-1012",10))
