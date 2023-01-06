14e888d9701ad0544961fae6563d35fdbf3a9e49
Yuragranovsky <yuragranovsky@gmail.com>
2023-01-06 12:05:13 +0200

    CW #15 with TODO's for HW #15

--- app.js
+++ app.js
@@ -1,89 +1,75 @@
-const array = [10, 20, 30, -10, 11, 100];
-const index = array.indexOf(30);
-//removing all numbers from 30
-//const res = array.splice(index);
-//array.splice(index, undefined, -1000);
-//insert
-//array.splice(index, 0, -10, -20);
-//replace
-//array.splice(index, 2, 20);
-// const ar1 = [40,200];
-// array.splice(index, 1, ...ar1);
-// const numbersPerPage = 2;
-// const page = 3;
-// const firstIndex = (page - 1) * numbersPerPage
-// const pageNumbers = array.slice(firstIndex,
-//      firstIndex + numbersPerPage )
-// const ar1 = array.map(multiply2);
-// function multiply2(number) {
-//     return number * 2;
+const array = [123, 9, 28, 3, 44, -123];
+array.sort(function(e1, e2){
+    let res = e1.toString().length - e2.toString().length;
+    if (!res) {
+        res = e1 - e2;
+    }
+    return res;
+});
+// console.log(`result of sorting [123, 9, 28, 3, 44] is ${array}`)
+//HW #15
+/************************************************************************************** */
+//task sort.1
+function evenOddSort(array) {
+    //TODO
+    //sort array of number in the order even numbers go before the odd ones
+    //example input array [20, -10,333,1000, 552, 7, -7] => [20, -10, 1000, 552, 333, 7, -7]
+}
+//task sort.2
+function oddEvenSort(array) {
+    //TODO
+    //sort array of number in the order even numbers go after the odd ones
+    //example input array [20, -10,333,1000, 552, 7, -7] => [333, 7, -7, 20, -10, 1000, 552]
+}
+//task sort.3
+function evenAscOddDesc(array) {
+    //TODO
+    //sort array of number in the order even numbers go before the odd ones
+    //even numbers should be in the ascending order, odd numbers should be in the descending
+    //example input array [20, -10,333,1000, 552, 7, -7] => [333, 7, -7, 20, -10, 1000, 552]
+}
+/*************************************************************************** */
+//reduce
+//find sum of the numbers in an array
+// function sum(array) {
+//     const res = array.reduce(function(res, cur){
+//         return res + cur;
+//     } );
+//     return res;
 // }
-// const ar2 = array.map(function (number, index) {
-//     return index + 1 + '. ' + number;
-// });
-// const ar3 = array.map(function(number) {
-//     return `<li>${number}</li>`
-// })
-// const ulElement = `<ul>${ar3.join('')}</ul>`
-//HW #14 (1)
-function coloringString(str1, str2) {
-    //str1 - string
-    //str2 - string
-    //the strings should be with the same length
-    //returns array of following colors
-    //if str2[i] exists in str1 but in another position - i-th color yellow
-    //if str2[i] == str1[i] - i-th color green
-    //if str2[i] doesn't exist in str1 - i -th color red
-    //For getting array from string there is the following expression Array.from(string)
-    //Examples: coloringString("pappy", "apple") => ["yellow","yellow", "green","red", "red"]
-    //coloringString("pappy", "pappy") => ["green", "green", "green", "green", "green"]
-    const arStr2 = Array.from(str2);
-    const res = arStr2.map(function(symbol, index) {
-        let color;
-        if (symbol === str1[index]) {
-            color = "green";
-        } else  {
-            color = str1.includes(symbol) ? "yellow" : "red";
-        }
-        return color;
-    })
-    return res;
-    
-}
-// console.log(`coloringString("pappy", "apple") returns ${coloringString("pappy", "apple").join(' ')}` );
-// console.log(`coloringString("pappy", "pappy") returns ${coloringString("pappy", "pappy")}` );
-//getting array of numbers in a given range. Example as follows 
-// const min = 5;
-// const max = 15;
-// const ar4 = array.filter(function(number) {
-//     return number >= min && number <= max;
-// })
-//getting new array of the elements of the source array with even indexes
-const ar5 = array.filter(function (__, index) {
-    return index % 2 === 0;
-})
-//HW #14 (2)
-function getNumbersWithDigitsAmount(digitsAmount, array) {
-    //takes: digitsAmount - given number of digits; array - any array of integer numbers
-    //returns array of only numbers from the given array comprising of the given number of digits
-    //examples: 
-    //getNumbersWithDigitsAmount(3, [1, 100, -100, 25, 1000]) returns [100, -100]
-   
-    const res = array.filter(function(number) {
-        const nDigits = getNumberDigits(number);
-        return nDigits === digitsAmount ;
-
-    });
-    return res;
-}
-function getNumberDigits(number) {
-    number = Math.abs(Math.trunc(number));
-    const res = number.toString().length;
-    return res;
-}
-console.log(`getNumbersWithDigitsAmount(3, [1, 100, -100, 25, 1000]) returns ${getNumbersWithDigitsAmount(3, [1, 100, -100, 25, 1000])}`)
-
-
-
-
-
+// console.log(`sum([1,2,3,4,5]) returns ${sum([1,2,3,4,5])}`);
+// console.log([1, 2, 3, 4].reduce(function (x, y) {
+//     console.log(x, y);
+//     return x + y;
+  
+// },5)); //1-function call x = 1, y = 2; 2-function call x = undefined, y = 3; 3-th x = undefined, y = 4
+console.log([[0, 1], [2, 3]].reduce(
+    (acc, cur) => {
+      return acc.concat(cur);
+    },
+    [1, 2]
+  ));
+  //1 - acc - [1, 2], cur - [0, 1]
+  //2 - acc -[1, 2, 0, 1], cur - [2, 3]
+  //HW #15
+  //task reduce.1
+  function getMin(array) {
+    //TODO
+    //find and returns minimal value of array
+  }
+  //task reduce.2
+  function getMax(array) {
+    //TODO
+    //find and returns minimal value of array
+  }
+  //task reduce.3
+  function getAverage(array) {
+    //TODO
+    //find and returns average value of array
+  }
+  //task reduce.4
+  function getMinMaxAvg(array) {
+    //TODO
+    //using one reduce call to get miniamal, maximal and average
+  }
+  
