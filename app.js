e4dfd8807c8196586bba3a9d0d8298a6721bfe4c
Yuragranovsky <yuragranovsky@gmail.com>
2023-01-10 18:05:12 +0200

    HW #15 Solution sort reduce

--- app.js
+++ app.js
@@ -1,75 +1,84 @@
-const array = [123, 9, 28, 3, 44, -123];
-array.sort(function(e1, e2){
-    let res = e1.toString().length - e2.toString().length;
-    if (!res) {
-        res = e1 - e2;
-    }
-    return res;
-});
-// console.log(`result of sorting [123, 9, 28, 3, 44] is ${array}`)
+
 //HW #15
+const array = [20, -10,333,1000, 552, 7, -7, 556, 555];
 /************************************************************************************** */
 //task sort.1
 function evenOddSort(array) {
-    //TODO
+    
     //sort array of number in the order even numbers go before the odd ones
     //example input array [20, -10,333,1000, 552, 7, -7] => [20, -10, 1000, 552, 333, 7, -7]
+    return array.sort(function(n1, n2) {
+      return n1 % 2 == 0 ? -1 : 1;
+    })
 }
 //task sort.2
 function oddEvenSort(array) {
-    //TODO
+    
     //sort array of number in the order even numbers go after the odd ones
     //example input array [20, -10,333,1000, 552, 7, -7] => [333, 7, -7, 20, -10, 1000, 552]
+    return array.sort(function(n1, n2) {
+     return n1 % 2 == 0 ? 1 : -1;
+});
 }
 //task sort.3
 function evenAscOddDesc(array) {
-    //TODO
+    
     //sort array of number in the order even numbers go before the odd ones
     //even numbers should be in the ascending order, odd numbers should be in the descending
     //example input array [20, -10,333,1000, 552, 7, -7] => [-10, 20, 552, 100, 333, 7, -7]
+    return array.sort(function(n1, n2) {
+      let res = Math.abs(n1 % 2) - Math.abs(n2 % 2);
+      if (res === 0) {
+        res = n1 % 2 == 0 ? n1 - n2 : n2 - n1;
+      }
+      return res;
+    })
 }
 /*************************************************************************** */
-//reduce
-//find sum of the numbers in an array
-// function sum(array) {
-//     const res = array.reduce(function(res, cur){
-//         return res + cur;
-//     } );
-//     return res;
-// }
-// console.log(`sum([1,2,3,4,5]) returns ${sum([1,2,3,4,5])}`);
-// console.log([1, 2, 3, 4].reduce(function (x, y) {
-//     console.log(x, y);
-//     return x + y;
-  
-// },5)); //1-function call x = 1, y = 2; 2-function call x = undefined, y = 3; 3-th x = undefined, y = 4
-console.log([[0, 1], [2, 3]].reduce(
-    (acc, cur) => {
-      return acc.concat(cur);
-    },
-    [1, 2]
-  ));
-  //1 - acc - [1, 2], cur - [0, 1]
-  //2 - acc -[1, 2, 0, 1], cur - [2, 3]
+
+ 
   //HW #15
   //task reduce.1
   function getMin(array) {
-    //TODO
+    
     //find and returns minimal value of array
+    return array.reduce(function(min, cur) {
+      return cur < min ? cur : min;
+    })
   }
   //task reduce.2
   function getMax(array) {
-    //TODO
     //find and returns minimal value of array
+    return array.reduce(function(max, cur) {
+      return cur > max ? cur : max;
+    })
   }
   //task reduce.3
   function getAverage(array) {
-    //TODO
-    //find and returns average value of array
+    const sum = array.reduce(function(sum, cur){
+      return sum + cur
+    });
+    return sum / array.length;
   }
   //task reduce.4
   function getMinMaxAvg(array) {
-    //TODO
-    //using one reduce call to get miniamal, maximal and average
+   
+    const res = array.reduce(function (ar, cur) {
+      if (cur < ar[0]) {
+        ar[0] = cur;
+      } else if (cur > ar[1]) {
+        ar[1] = cur;
+      }
+      ar[2] += cur;
+      return ar;
+    }, [array[0], array[0], 0]);
+    res[2] /= array.length;
+    return res;
   }
-  
+  console.log(`array: [${array}] evenOddSort => [${evenOddSort(array)}]`);
+  console.log(`array: [${array}] oddEvenSort => [${oddEvenSort(array)}]`);
+  console.log(`array: [${array}] evenAscOddDescSort => [${evenAscOddDesc(array)}]`);
+  console.log(`array: [${array}] getMin => ${getMin(array)}`);
+  console.log(`array: [${array}] getMax => ${getMax(array)}`);
+  console.log(`array: [${array}] getAverage => ${getAverage(array)}`);
+  console.log(`array: [${array}] getMinMaxAvg => [${getMinMaxAvg(array)}]`);
