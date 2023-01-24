c9a34c88fade47a8ea7374c7e484c439b15cc1e6
Yuragranovsky <yuragranovsky@gmail.com>
2023-01-24 21:12:11 +0200

    CW #20

---
+++ src/movies.js
@@ -0,0 +1,48 @@
+// const employee1 = {id: 123, address: {city: "Lod", country: "Israel"}};
+// const employee2 = {id: 123, address: {city: "Lod", country: "Israel"}};
+// const employee1Json = JSON.stringify(employee1);
+// const employee2Json = JSON.stringify(employee2);
+// console.log(employee1Json);
+// const employee3 = {...employee1};
+// // employee3.id = 124;
+// // employee3.address.city = "Rehovot";
+// // console.log(employee1.address.city);
+// const employee4 = JSON.parse(employee1Json);
+// employee4.address.city = 'Rehovot';
+// console.log(employee1.address.city);
+// console.log(employee4.address.city);
+const jsonData = '{"page":1,"results":[{"adult":false,"backdrop_path":"/bQXAqRx2Fgc46uCVWgoPz5L5Dtr.jpg","genre_ids":[28,14,878],"id":436270,"original_language":"en","original_title":"Black Adam","overview":"Nearly 5,000 years after he was bestowed with the almighty powers of the Egyptian gods—and imprisoned just as quickly—Black Adam is freed from his earthly tomb, ready to unleash his unique form of justice on the modern world.","popularity":15075.276,"poster_path":"/pFlaoHTZeyNkG83vxsAJiGzfSsa.jpg","release_date":"2022-10-19","title":"Black Adam","video":false,"vote_average":7.2,"vote_count":1888},{"adult":false,"backdrop_path":"/kmzppWh7ljL6K9fXW72bPN3gKwu.jpg","genre_ids":[14,28,35,80],"id":1013860,"original_language":"en","original_title":"R.I.P.D. 2: Rise of the Damned","overview":"When Sheriff Roy Pulsipher finds himself in the afterlife, he joins a special police force and returns to Earth to save humanity from the undead.","popularity":6040.388,"poster_path":"/g4yJTzMtOBUTAR2Qnmj8TYIcFVq.jpg","release_date":"2022-11-15","title":"R.I.P.D. 2: Rise of the Damned","video":false,"vote_average":6.8,"vote_count":152},{"adult":false,"backdrop_path":"/kpUre8wWSXn3D5RhrMttBZa6w1v.jpg","genre_ids":[35,10751,14],"id":338958,"original_language":"en","original_title":"Disenchanted","overview":"Disillusioned with life in the city, feeling out of place in suburbia, and frustrated that her happily ever after hasn’t been so easy to find, Giselle turns to the magic of Andalasia for help. Accidentally transforming the entire town into a real-life fairy tale and placing her family’s future happiness in jeopardy, she must race against time to reverse the spell and determine what happily ever after truly means to her and her family.","popularity":3316.598,"poster_path":"/4x3pt6hoLblBeHebUa4OyiVXFiM.jpg","release_date":"2022-11-16","title":"Disenchanted","video":false,"vote_average":7.5,"vote_count":352}]}'
+const httpPrefix = "https://image.tmdb.org/t/p/w500";
+const data = JSON.parse(jsonData);
+const bodyElement = document.querySelector("ul");
+const detailsImageElement = document.getElementById("details-image");
+bodyElement.innerHTML += getAnchors()
+const anchorElements = document.querySelectorAll("a");
+function showDetails(index) {
+    console.log(`${httpPrefix}${data.results[index].backdrop_path}`)
+    detailsImageElement.src=`${httpPrefix}${data.results[index].backdrop_path}`
+}
+anchorElements.forEach((anchor, index) =>
+ anchor.addEventListener("click",function(){
+    showDetails(index);
+ }))
+function getAnchors() {
+    return `${getListItems()}`
+}
+function getListItems() {
+    const itemsArray = data.results.map(movie => 
+        `<li>
+            <a href="#"> 
+                <img src="${httpPrefix}${movie.poster_path}">
+                <span>
+                   ${movie.original_title}
+                </span>
+            </a>
+        </li>`);
+    return itemsArray.join('');
+}
+
+
+
+
+
