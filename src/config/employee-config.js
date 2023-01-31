bb8d64df70ec6f5a7e46620ff83635f27c5a44fc
Yuragranovsky <yuragranovsky@gmail.com>
2023-01-31 21:37:46 +0200

    CW #22 employeeForm addEmployee Company

---
+++ src/config/employee-config.js
@@ -0,0 +1,8 @@
+export const employeeConfig = {
+    minId: 100000000,
+    maxId: 999999999,
+    countries: {
+        "Israel": ["Lod", "Tel Aviv", "Rehovot"],
+        "USA": ["New York", "Boston", "Vashington"]
+    }
+}
--- src/main.js
+++ src/main.js
@@ -1,2 +1,17 @@
 import { Company, createEmployee } from "./service/company.js";
+import { EmployeeForm } from "./ui/employee-form.js";
+import { getRandomNumber } from "./utils/random.js";
+const MIN_ID = 100000000;
+const MAX_ID = 999999999;
+const company = new Company();
+const employeeForm = new EmployeeForm("form-section")
+function addEmployee(employeeData) {
+    const id = getRandomNumber(MIN_ID, MAX_ID);
+    const employee = createEmployee(id, employeeData.name,
+        +employeeData.birthYear, +employeeData.salary,
+        employeeData.city, employeeData.country);
+    company.addEmployee(employee);
+    console.log(employee)
+}
+employeeForm.addFormHandler(addEmployee)
 
