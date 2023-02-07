9949f400aaed8696a6c5f16e16da3c044b32e6f1
Yuragranovsky <yuragranovsky@gmail.com>
2023-02-07 21:12:47 +0200

    CW #24 Promise introduction, util function "sleep"

--- src/main.js
+++ src/main.js
@@ -1,35 +1,14 @@
-import { Company, createEmployee } from "./service/company.js";
-import { EmployeeForm } from "./ui/employee-form.js";
-import { Table } from "./ui/table.js";
-import { Tabs } from "./ui/tabs.js";
-
-const schema = [
-    {columnName: 'Employee ID', fieldName: 'id'},
-    {columnName: 'Name', fieldName: 'name'},
-    {columnName: "Birth Year", fieldName: 'birthYear'},
-    {columnName: "Salary (NIS)", fieldName: 'salary'},
-    {columnName: "Country", fieldName: 'country'},
-    {columnName: "City", fieldName: 'city'}
-]
-const company = new Company();
-const employeeForm = new EmployeeForm("form-section");
-const tableEmployees = new Table("table-section", "Employees", schema);
-const tabs = new Tabs("menu", [{id: "form-section", name: "Add Employee"},
-{id: "table-section", name: "Show Employees"},
-{id: "query-section", name: "Search Employees"} ])
-function addEmployee(employeeData) {
+import { sleep } from "./utils/sleep.js";
+const TIMEOUT = 5000;
+// const promise = sleep(TIMEOUT);
+// promise.then(() => console.log(`after ${TIMEOUT/1000} seconds`));
+// 
+ const fun = async () => {
    
-    const employee = createEmployee(employeeData.name,
-        +employeeData.birthYear, +employeeData.salary,
-        employeeData.city, employeeData.country);
-        const res = company.addEmployee(employee);
-        if (!res.message) {
-            employeeData.id = res.id;
-            tableEmployees.addRow(employeeData);
-        }
-        return res.message
-    
+await sleep(TIMEOUT);
+console.log(`after ${TIMEOUT/1000} seconds`)
 }
-employeeForm.addFormHandler(addEmployee);
-
+fun();
+console.log(`waiting for ${TIMEOUT/1000} seconds`)
+console.log("*****************************************")
 
